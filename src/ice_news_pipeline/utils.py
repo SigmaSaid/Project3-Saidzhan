f871cd8a697a13fe36311ec1f500d6c75ed2de5c
SigmaSaid <saitovsaid22@gmail.com>
2026-08-03 16:58:23 +0600

    Update utils.py

--- src/ice_news_pipeline/utils.py
+++ src/ice_news_pipeline/utils.py
@@ -6,9 +6,6 @@ from bs4 import BeautifulSoup, Tag
 from ice_news_pipeline.constants import BODY_SELECTOR
 from ice_news_pipeline.normalize import (
     iso_date,
-    normalize_text,
-    normalize_url,
-    tokens,
 )
 
 _TITLE_SUFFIX_RE = re.compile(
